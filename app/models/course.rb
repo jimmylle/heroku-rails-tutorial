279539b3a0134e43b6b4b8285c8e19aa90954bee
jimmy le <lejimmyl@yahoo.com>
2016-01-09 22:01:44 -0800

    Added a test and course_num method

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ActiveRecord::Base
+  def course_num
+    return "#{dept} #{num}"
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Course, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:course_instance) { Course.create(dept: "Math", num: "3A", desc: "Calculus") }
+  describe "public instance methods" do
+    context "responds to its methods" do
+      it { expect(course_instance).to respond_to(:course_num) }
+    end
+    it "course_num should return Math 3A" do
+      expect(course_instance.course_num).to eq("Math 3A")
+    end
+  end
 end
